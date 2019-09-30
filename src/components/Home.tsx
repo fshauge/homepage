7b392a1eb5921fac33f16e99879183de53cd2aa5
Markus Hauge <markus.hauge@hotmail.com>
2019-09-30 02:05:27 +0200

    Add "work in progress" to home page

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,3 +1,11 @@
-const Home = () => null;
+import React from "react";
+
+const Home = () => (
+  <div className="jumbo">
+    <section className="container">
+      <h1>🚧 Work in progress</h1>
+    </section>
+  </div>
+);
 
 export default Home;
