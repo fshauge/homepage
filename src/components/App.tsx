30d1972d6cbccdc39952741dc3471ed9fe4eace4
Markus Hauge <markus.hauge@hotmail.com>
2019-09-30 18:27:27 +0200

    Use BrowserRouter instead of HashRouter

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,10 @@
 import React from "react";
-import { HashRouter as Router, NavLink, Route, Switch } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  NavLink,
+  Route,
+  Switch
+} from "react-router-dom";
 import Home from "./Home";
 import Profile from "./Profile";
 
